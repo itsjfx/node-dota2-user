c5ffc9c3ee33d718894d5b20c5f83a14b7ea388e
Thomas <me@jfx.ac>
2023-04-22 00:38:01 +1000

    a somewhat functional router for handling messages

--- src/Dota2User.ts
+++ src/Dota2User.ts
@@ -1,5 +1,7 @@
-import { EventEmitter } from 'events';
+import { EventEmitter } from 'node:events';
 import SteamUser from 'steam-user';
+import { Router } from './Router';
+import { EGCBaseClientMsg } from './protobufs/gcsystemmsgs';
 const Protos = require('../protobufs/generated/_load.js'); // TODO move to src
 
 // TODO... there has to be a better way
@@ -24,6 +26,7 @@ export class Dota2User extends EventEmitter {
     // TODO
     _helloTimer: NodeJS.Timer | undefined | null;
     _helloTimerMs: number | undefined;
+    router: Router;
 
     constructor(steam: SteamUser) {
         if (steam.packageName !== 'steam-user' || !('packageVersion' in steam) || !steam.constructor) {
@@ -35,22 +38,24 @@ export class Dota2User extends EventEmitter {
             }
         }
 
+        // TODO: EventEmitter args
         super();
+        this.router = new Router();
         this._steam = steam;
         this.haveGCSession = false;
         this._inDota2 = false;
 
         // Hook our steam-user events
+        // TODO should this be happening in the constructor or ...
         this._steam.on('receivedFromGC', (appid, msgType, payload) => {
             if (appid !== Dota2User.STEAM_APPID) {
                 return; // we don't care
             }
-    
-            this._handleMessage(msgType, payload);
+            this.router.route(msgType, payload);
         });
 
         this._steam.on('appLaunched', (appid) => {
-            if (this._inDota2 || appid !== Dota2User.STEAM_APPID) {
+            if (this.inDota2 || appid !== Dota2User.STEAM_APPID) {
                 return;
             }
     
@@ -61,7 +66,7 @@ export class Dota2User extends EventEmitter {
         });
 
         this._steam.on('appQuit', (appid) => {
-            if (!this._inDota2 || appid !== Dota2User.STEAM_APPID) {
+            if (!this.inDota2 || appid !== Dota2User.STEAM_APPID) {
                 return;
             }
     
@@ -75,6 +80,20 @@ export class Dota2User extends EventEmitter {
         this._steam.on('error', (err) => {
             this._handleAppQuit(true);
         });
+
+        this.router.on(EGCBaseClientMsg.k_EMsgGCClientWelcome.toString(), (message) => {
+            // Handle caches
+
+            // this.inventory = this.inventory || [];
+            this.emit('debug', "GC connection established");
+            this.haveGCSession = true;
+            this._clearHelloTimer();
+            this.emit('connectedToGC');
+        });
+    }
+
+    get inDota2() {
+        return this._inDota2;
     }
 }
 // @ts-ignore
