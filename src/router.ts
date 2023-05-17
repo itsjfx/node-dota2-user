57d5f21fe6f98497e60d6d08bf856d80d9ddd24a
Thomas <me@jfx.ac>
2023-05-17 22:44:29 +1000

    should totally not crash when it cant route

--- src/router.ts
+++ src/router.ts
@@ -4,18 +4,20 @@ import { ExtendedEventEmitter, TypedEmitter, getProtobufForMessage, Dota2UserErr
 import { ProtobufEvents } from './known-protobufs';
 
 export class Router extends (ExtendedEventEmitter as new () => TypedEmitter<ProtobufEvents>) {
-    route(messageId: number, body: Buffer) {
+    route(messageId: number, body: Buffer): void {
         // let msgName = getMessageName(msgType) || msgType;
         // TODO when we import all the protos, find message name instead of printing just the messageId
+        let protobuf: ReturnType<typeof getProtobufForMessage>;
         try {
-            const protobuf = getProtobufForMessage(messageId);
-            const data = protobuf.decode(body);
-            this.emit(messageId as keyof ProtobufEvents, data as any);
+            protobuf = getProtobufForMessage(messageId);
         } catch (err) {
             if (err instanceof Dota2UserError) {
                 debug('No route available for GC message %s', messageId);
+                return;
             }
             throw err;
         }
+        const data = protobuf.decode(body);
+        this.emit(messageId as keyof ProtobufEvents, data as any);
     }
 }
