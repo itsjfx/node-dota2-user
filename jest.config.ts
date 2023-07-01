e3228c2391214ec26fca0d6346d8030268059ca8
Thomas <me@jfx.ac>
2023-07-02 02:13:23 +1000

    test: add jest and test constructor of Dota2User

---
+++ jest.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/test'],
+    setupFilesAfterEnv: [
+        '<rootDir>/test/index.ts',
+    ],
+    testMatch: ['**/*.test.ts'],
+    testPathIgnorePatterns: [
+    ].filter((file) => !process.argv.includes(file)),
+    transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+            isolatedModules: process.env.FAST_JEST=='1',
+        }],
+    },
+    resetMocks: true,
+    reporters: [
+        ['default', {summaryThreshold: 0}],
+        'github-actions',
+    ],
+    coverageReporters: [['text', { skipFull: true }]],
+    coverageDirectory: '.',
+    collectCoverageFrom: [
+        '**/*.ts',
+        '!test/**',
+        // TODO
+        '!src/bin/**',
+        '!src/protobufs/**',
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
+
+export default config;
---
+++ test/src/Dota2User.test.ts
@@ -0,0 +1,38 @@
+import SteamUser from "steam-user";
+import { Dota2User, Dota2UserError } from "../../src";
+
+describe('Dota2User', () => {
+    describe('constructor()', () => {
+        // not even bothering to test all the cases here, cause I really don't believe it's necessary
+        describe('where a completely different class instance is given', () => {
+            test('throw an error', () => {
+                const blargh = new class {} as SteamUser;
+                expect(() => new Dota2User(blargh)).toThrow(Dota2UserError);
+            });
+        });
+        describe('where an valid SteamUser instance is given', () => {
+            let client: SteamUser;
+            const clientPackageVersionSpy = jest.spyOn(SteamUser.prototype, 'packageVersion', 'get');
+            beforeEach(() => {
+                client = new SteamUser();
+            });
+            test('throw an error if the major version is less than 4', () => {
+                clientPackageVersionSpy.mockReturnValue('3.0.0');
+                expect(() => new Dota2User(client)).toThrow(Dota2UserError);
+            });
+            test('throw an error if the minor version is less than 2 where major is at least 4', () => {
+                clientPackageVersionSpy.mockReturnValue('4.1.0');
+                expect(() => new Dota2User(client)).toThrow(Dota2UserError);
+            });
+            test('dont throw an error if package is > 4.2.0', () => {
+                clientPackageVersionSpy.mockReturnValue('4.2.1');
+                expect(() => new Dota2User(client)).not.toThrow(Dota2UserError);
+            });
+            test.each(['_hookSteamUserEvents', '_hookRouterEvents'])('%s should be called', (functionName) => {
+                const spy = jest.spyOn(Dota2User.prototype, functionName as any);
+                new Dota2User(client);
+                expect(spy).toBeCalled();
+            });
+        });
+    });
+});
