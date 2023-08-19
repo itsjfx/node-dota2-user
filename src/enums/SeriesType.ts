2e36bcf427371c78d1efc6acaca7e1f039acf1e0
Thomas <me@jfx.ac>
2023-08-20 00:22:38 +1000

    SeriesType: add SeriesType enum

---
+++ src/enums/SeriesType.ts
@@ -0,0 +1,7 @@
+// from node-dota2
+// https://github.com/Arcana/node-dota2/blob/5fe38174b0c61840e3b379d2dd8320a54ac03486/handlers/helper.js#L61
+export enum SeriesType {
+    NONE = 0,
+    BEST_OF_THREE = 1,
+    BEST_OF_FIVE = 2,
+}
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export { Dota2User } from './Dota2User';
 export { Dota2UserError } from './utils';
 export { ServerRegion } from './enums/ServerRegion';
+export { SeriesType } from './enums/SeriesType';
